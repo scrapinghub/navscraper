ebe845d798803d318441f206ff1d4163ac406be2
Rolando Espinoza <rolando@scrapinghub.com>
2013-10-06 03:00:08 -0400

    Initial scrapy project.

---
+++ navscraper/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for navscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'navscraper'
+
+SPIDER_MODULES = ['navscraper.spiders']
+NEWSPIDER_MODULE = 'navscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'navscraper (+http://www.yourdomain.com)'
